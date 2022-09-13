48b9990ddae0950d113705e9178a46a2bb9be13b
marek9911 <53997336+marek9911@users.noreply.github.com>
2022-09-13 19:16:40 +0200

    Complete rewrite
    Complete class rewrite, added new functions

--- Memory/Memory.cpp
+++ Memory/Memory.cpp
@@ -2,45 +2,66 @@
 #include "Console/Console.h"
 #endif
 #include "Memory.h"
+#include <Psapi.h>
 #include <iostream>
 
-using namespace std;
-
-Memory::Memory(unsigned int pointerAddress, std::vector<unsigned int> pointerOffsets)
-: address((DWORD*)(*(DWORD*)((uintptr_t)GetModuleHandleA("goofball.exe") + pointerAddress))) {
-    if (pointerOffsets.size() > 1)
-        for (unsigned int i = 0; i < pointerOffsets.size() - 1; i++)
-            address = (DWORD*)(*(DWORD*)(address + pointerOffsets[i] / 4));
-    address += pointerOffsets[pointerOffsets.size() - 1] / 4;
-    #ifdef CONSOLE_H
-    Console::ChangeColor(lightBlue);
-    #endif
-    std::cout << "New pointer has been attached to 0x" << address << endl;
+uintptr_t Memory::GetBaseAddress() {
+	return baseAddress;
 }
 
-void Memory::Move(int offset) {
-    address += offset;
-    #ifdef CONSOLE_H
-    Console::ChangeColor(blue);
-    #endif
-    std::cout << "Pointer moved from 0x" << address - offset << " to 0x" << address << endl;
+void Memory::HexWrite(uintptr_t pointerWithoutBase, const char* bytes, size_t size) {
+	#pragma warning(disable:6387)
+	uintptr_t ptr = GetBaseAddress() + pointerWithoutBase;
+	size_t byteSize = size;
+	if (byteSize == 0) {
+		byteSize = strlen(bytes) + 1;
+	}
+	unsigned long OldProtection;
+	VirtualProtect((LPVOID)(ptr), byteSize, PAGE_EXECUTE_READWRITE, &OldProtection);
+	memcpy((LPVOID)ptr, bytes, byteSize);
+	VirtualProtect((LPVOID)(ptr), byteSize, OldProtection, NULL);
+	#pragma warning(default:6387)
 }
 
-void Memory::Patch(int value) {
-    *(int*)address = value;
-    #ifdef CONSOLE_H
-    Console::ChangeColor(lightYellow);
-    #endif
-    std::cout << "Memory at 0x" << address << " changed to 0x" << uppercase << hex << value << dec << endl;
+void Memory::hexDump(void* ptr, size_t size) {
+	unsigned char* chptr = (unsigned char*)ptr;
+	__tcout << std::hex << std::uppercase;
+	for (size_t i = 0; i < size; i++) {
+		if (*chptr < 0x10) {
+			__tcout << __tstr("0");
+		}
+		__tcout << (int)*(chptr++) << __tstr(" ");
+	}
+	__tcout << std::dec << std::endl;
 }
 
-void Memory::Display() {
-    #ifdef CONSOLE_H
-    Console::ChangeColor(lightPurple);
-    #endif
-    cout << "Memory at 0x" << address << " is 0x" << uppercase << hex << *(int*)address << endl;
+bool Memory::CheckReadableAddress(uintptr_t address) {
+	MEMORY_BASIC_INFORMATION mbi;
+	VirtualQuery((LPCVOID)address, &mbi, sizeof(mbi));
+	if (mbi.Protect > 0x01) {
+		return false;
+	}
+	return true;
 }
 
-int Memory::GetValue() {
-    return *(int*)address;
+bool Memory::CheckWritableAddress(uintptr_t address) {
+	MEMORY_BASIC_INFORMATION mbi;
+	VirtualQuery((LPCVOID)address, &mbi, sizeof(mbi));
+	if (mbi.Protect == 0x04 || mbi.Protect == 0x08 || mbi.Protect == 0x40 || mbi.Protect == 0x80) {
+		return false;
+	}
+	return true;
 }
+
+uintptr_t Memory::CalculateBaseAddress() {
+	HANDLE hpr = OpenProcess(PROCESS_ALL_ACCESS, FALSE, GetCurrentProcessId());
+	LPWSTR pname = new WCHAR[1024];
+	GetProcessImageFileNameW(hpr, pname, 1024);
+	std::wstring path = pname;
+	delete[] pname;
+	path = path.substr(path.find_last_of(L"/\\") + 1);
+	return (uintptr_t)GetModuleHandleW(path.c_str());
+}
+
+uintptr_t Memory::baseAddress = CalculateBaseAddress();
+
--- Memory/Memory.h
+++ Memory/Memory.h
@@ -3,18 +3,149 @@
 #define MEMORY_H
 #include <vector>
 #include <Windows.h>
-using namespace std;
+#include <iostream>
+
+#if __has_include("Console/Console.h")
+#include "Console/Console.h"
+#elif __has_include("Console.h")
+#include "Console.h"
+#endif
+
+// Uncomment macro under this line to print debug information (if enabled) using wide characters
+//#define MEMORY_WIDE_OUTPUT
+#ifndef MEMORY_WIDE_OUTPUT
+#define __tcout std::cout
+#define __tstr(x) x
+#define __tprintf printf
+#define __tvprinf vprintf
+typedef const char* __TCHAR;
+const unsigned int msgTypes[4] = { 0x5D2B5B, 0x5D3D5B, 0x5D1D5B, 0x5D215B };
+#else
+#define __tcout std::wcout
+#define __tstr(x) L##x
+#define __tprintf wprintf
+#define __tvprinf vwprintf
+typedef const wchar_t* __TCHAR;
+const unsigned long long msgTypes[4] = { 0x5D002B005B, 0x5D003D005B, 0x5D2194005B, 0x5D0021005B };
+#endif
+
+#define MEMORY_SHOW_DEBUG true
+#define MSG_OBJECT_NEW 0
+#define MSG_OBJECT_CHANGE 1
+#define MSG_OBJECT_MOVE 2
+#define MSG_OBJECT_FAIL 3
 
 class Memory
 {
 	public:
-		Memory(unsigned int pointerAddress, vector<unsigned int> pointerOffsets);
-		void Move(int offset);
-		void Patch(int value);
-		void Display();
-		int GetValue();
-	private:
-		const DWORD* address;
-};
+		template <class T> class Object {
+			public:
+				// Creates pointer to any object in memory
+				Object(uintptr_t pointerAddress, std::vector<intptr_t> pointerOffsets, bool printDebug = false)
+				: defaultPointerAddress(pointerAddress), pointerOffsets(pointerOffsets), printDebug(printDebug) {
+					this->Refresh();
+				}
 
+				// Refreshes address of object pointer (returns 0 if address success)
+				bool Refresh() {
+					uintptr_t tempAddress = baseAddress + defaultPointerAddress;
+
+					for (size_t i = 0; i < pointerOffsets.size(); i++) {
+						if (!CheckReadableAddress(tempAddress)) {
+							tempAddress = (*(uintptr_t*)tempAddress) + pointerOffsets[i];
+						}
+						else {
+							PrintDebug(MSG_OBJECT_FAIL, __tstr("Memory object initialization failed: Address is not valid"));
+							return true;
+						}
+					}
+					if (!CheckWritableAddress(tempAddress)) {
+						PrintDebug(MSG_OBJECT_NEW, __tstr("New memory object has been attached to 0x%p"), (T*)tempAddress);
+						address = (T*)tempAddress;
+						return false;
+					}
+					PrintDebug(MSG_OBJECT_FAIL, __tstr("Memory object initialization failed: Address is not writable"));
+					return true;
+				}
+
+				// Changes value of object pointer (returns 0 if address success)
+				bool ChangeValue(T value) {
+					if (!CheckWritableAddress((uintptr_t)address)) {
+						PrintDebug(MSG_OBJECT_CHANGE, __tstr("Value of memory object at 0x%p has been changed"), address);
+						*address = value;
+						return false;
+					}
+					PrintDebug(MSG_OBJECT_FAIL, __tstr("Memory object value change failed: Address is not writable!"));
+					return true;
+				}
+
+				// Moves object pointer (refresh will return object pointer to default address) (returns 0 if address success)
+				bool Move(intptr_t offset) {
+					uintptr_t tempAddress = (uintptr_t)address;
+					tempAddress += offset;
+					if (!CheckWritableAddress(tempAddress)) {
+						PrintDebug(MSG_OBJECT_MOVE, __tstr("Memory object has been moved from 0x%p to 0x%p"), (T*)(tempAddress - offset), (T*)tempAddress);
+						address = (T*)tempAddress;
+						return false;
+					}
+					PrintDebug(MSG_OBJECT_FAIL, __tstr("Memory object move failed: Address is not writable!"));
+					return true;
+				}
+
+				// Returns value of object pointer
+				T GetValue() {
+					if (!CheckReadableAddress((uintptr_t)address)) {
+						return *address;
+					}
+					PrintDebug(MSG_OBJECT_FAIL, __tstr("Getting value from memory object failed: Address is not readable!"));
+					return NULL;
+				}
+
+				// Returns address of object pointer
+				uintptr_t GetAddress() {
+					return (uintptr_t)address;
+				}
+			private:
+				T* address;
+				const bool printDebug;
+				const uintptr_t defaultPointerAddress;
+				const std::vector<intptr_t> pointerOffsets;
+				void PrintDebug(int msgType, __TCHAR format, ...) {
+					if (printDebug) {
+						const unsigned int msgColors[4] = {0xA, 0xE, 0x9, 0xC};
+						va_list argptr;
+						va_start(argptr, format);
+						#ifdef CONSOLE_H
+						Console::ChangeColor(msgColors[msgType]);
+						#endif
+						__tprintf(__tstr("%s "), (__TCHAR)&msgTypes[msgType]);
+						#ifdef CONSOLE_H
+						Console::ChangeColor(WHITE);
+						#endif
+						__tvprinf(format, argptr);
+						__tprintf(__tstr("\n"));
+						va_end(argptr);
+					}
+				}
+		};
+		
+		// Returns base address of application
+		static uintptr_t GetBaseAddress();
+
+		// Hex write to address that may have read only access
+		static void HexWrite(uintptr_t pointerWithoutBase, const char* bytes, size_t size = 0);
+
+		// Prints hex dump of address
+		static void hexDump(void* ptr, size_t size);
+
+		// Checks if given address is readable (returns 0 if address is readable)
+		static bool CheckReadableAddress(uintptr_t address);
+
+		// Checks if given address is writable (returns 0 if address is writable)
+		static bool CheckWritableAddress(uintptr_t address);
+
+	private:
+		static uintptr_t CalculateBaseAddress();
+		static uintptr_t baseAddress;
+};
 #endif
