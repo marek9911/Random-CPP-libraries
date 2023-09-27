2fc506e94de14054ebe6d3ce2dcacf5ea9d3e5e0
marek9911 <53997336+marek9911@users.noreply.github.com>
2023-09-28 00:15:20 +0200

    Update

--- Console/Console.cpp
+++ Console/Console.cpp
@@ -1,6 +1,8 @@
 #include "Console.h"
 #include <iostream>
 #include <conio.h>
+#include <io.h>
+#include <fcntl.h>
 
 void Console::CreateNewConsole() {
 	AllocConsole();
@@ -31,17 +33,23 @@ void Console::SetCursorPos(short x, short y) {
 
 int Console::ReadKey() {
 	int keyValue = _getch();
-	if (keyValue == 224)
+	if (keyValue == 224) {
 		keyValue += _getch();
+	}
 	return keyValue;
 }
 
 void Console::AsyncReadKey(int* output, int* condition, int valueToStop) {
 	while (*condition != valueToStop){
-		if (_kbhit())
+		if (_kbhit()) {
 			*output = ReadKey();
+		}
 		Sleep(10);
 	}
+}
+
+void Console::BreakReadKey() {
+	_ungetch(0);
 }
 
 void Console::HideCursor(bool hideCursor) {
@@ -52,17 +60,35 @@ void Console::HideCursor(bool hideCursor) {
 }
 
 void Console::HideScrollBars(bool hideScrollBars) {
-	if (hideScrollBars)
+	if (hideScrollBars) {
 		ShowScrollBar(GetConsoleWindow(), SB_BOTH, ESB_ENABLE_BOTH);
-	else
+	}
+	else {
 		ShowScrollBar(GetConsoleWindow(), SB_BOTH, ESB_DISABLE_BOTH);
+	}
 }
 
-void Console::FixAnsiCodes()
-{
+void Console::EnableAnsiEscapeCodes() {
 	DWORD consoleMode;
-	if (GetConsoleMode(hConsole, &consoleMode))
+	if (GetConsoleMode(hConsole, &consoleMode)) {
 		SetConsoleMode(hConsole, consoleMode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+	}
+}
+
+void Console::EnableUTF8Output() {
+	SetConsoleOutputCP(65001);
+}
+
+void Console::EnableUTF8Input() {
+	SetConsoleCP(65001);
+}
+
+void Console::EnableWCHAROutput() {
+	int nul = _setmode(_fileno(stdout), _O_U16TEXT);
+}
+
+void Console::EnableWCHARInput() {
+	int nul = _setmode(_fileno(stdin), _O_U16TEXT);
 }
 
 HANDLE Console::hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
--- Console/Console.h
+++ Console/Console.h
@@ -1,45 +1,9 @@
 ﻿#pragma once
 #ifndef CONSOLE_H
 #define CONSOLE_H
-
-/*#ifdef UNICODE
-	#define BOX_DN_R u8"╔"	// ╔
-	#define BOX_UP_R u8"╚"	// ╚
-	#define BOX_DN_L u8"╗"	// ╗
-	#define BOX_UP_L u8"╝"	// ╝
-	#define BOX_VT u8"║"	// ║
-	#define BOX_HT u8"═"	// ═
-	#define LINE_DN_R u8"┌"	// ┌
-	#define LINE_UP_R u8"└"	// └
-	#define LINE_DN_L u8"┐"	// ┐
-	#define LINE_UP_L u8"┘"	// ┘
-	#define LINE_VT u8"│"	// │
-	#define LINE_HT u8"─"	// ─
-#else*/
-	#define BOX_DN_R char(201)	// ╔
-	#define BOX_UP_R char(200)	// ╚
-	#define BOX_DN_L char(187)	// ╗
-	#define BOX_UP_L char(188)	// ╝
-	#define BOX_VT char(186)	// ║
-	#define BOX_HT char(205)	// ═
-	#define BOX_3W_R char(204)	// ╠
-	#define BOX_3W_L char(185)	// ╣
-	#define BOX_3W_U char(202)	// ╩
-	#define BOX_3W_D char(203)	// ╦
-	#define BOX_4W char(206)	// ╬
-
-	#define LINE_DN_R char(218)	// ┌
-	#define LINE_UP_R char(192)	// └
-	#define LINE_DN_L char(191)	// ┐
-	#define LINE_UP_L char(217)	// ┘
-	#define LINE_VT char(179)	// │
-	#define LINE_HT char(196)	// ─
-	#define LINE_3W_R char(195)	// ├
-	#define LINE_3W_L char(180)	// ┤
-	#define LINE_3W_U char(193)	// ┴
-	#define LINE_3W_D char(194)	// ┬
-	#define LINE_4W char(197)	// ┼
-//#endif
+#if __has_include("Ascii.h")
+#include "Ascii.h"
+#endif
 
 #define BLACK 0x0
 #define BLUE 0x1
@@ -58,47 +22,6 @@
 #define LIGHTYELLOW 0xE
 #define BRIGHTWHITE 0xF
 
-#define ANSI_RESET "\033[0m"
-#define ANSI_BOLD "\033[1m"
-#define ANSI_UNDERLINE "\033[4m"
-#define ANSI_INVERSE "\033[7m"
-
-#define ANSI_BLACK "\033[30m"
-#define ANSI_RED "\033[31m"
-#define ANSI_GREEN "\033[32m"
-#define ANSI_YELLOW "\033[33m"
-#define ANSI_BLUE "\033[34m"
-#define ANSI_PURPLE "\033[35m"
-#define ANSI_CYAN "\033[36m"
-#define ANSI_WHITE "\033[37m"
-
-#define ANSI_LIGHTBLACK "\033[90m"
-#define ANSI_LIGHTRED "\033[91m"
-#define ANSI_LIGHTGREEN "\033[92m"
-#define ANSI_LIGHTYELLOW "\033[93m"
-#define ANSI_LIGHTBLUE "\033[94m"
-#define ANSI_LIGHTPURPLE "\033[95m"
-#define ANSI_LIGHTCYAN "\033[96m"
-#define ANSI_LIGHTWHITE "\033[97m"
-
-#define ANSI_BLACK_B "\033[40m"
-#define ANSI_RED_B "\033[41m"
-#define ANSI_GREEN_B "\033[42m"
-#define ANSI_YELLOW_B "\033[43m"
-#define ANSI_BLUE_B "\033[44m"
-#define ANSI_PURPLE_B "\033[45m"
-#define ANSI_CYAN_B "\033[46m"
-#define ANSI_WHITE_B "\033[47m"
-
-#define ANSI_LIGHTBLACK_B "\033[100m"
-#define ANSI_LIGHTRED_B "\033[101m"
-#define ANSI_LIGHTGREEN_B "\033[102m"
-#define ANSI_LIGHTYELLOW_B "\033[103m"
-#define ANSI_LIGHTBLUE_B "\033[104m"
-#define ANSI_LIGHTPURPLE_B "\033[105m"
-#define ANSI_LIGHTCYAN_B "\033[106m"
-#define ANSI_LIGHTWHITE_B "\033[107m"
-
 #define KB_ARROW_UP 296
 #define KB_ARROW_DOWN 304
 #define KB_ARROW_LEFT 299
@@ -106,10 +29,17 @@
 #define KB_ESCAPE 27
 #define KB_ENTER 13
 #define KB_RETURN 13
+#define KB_SPACE 32
+#define KB_BACKSPACE 8
+#define KB_TAB 9
 
-#include <tchar.h>
 #include <string>
+#ifdef _WIN32
+#include <tchar.h>
 #include <Windows.h>
+#else
+
+#endif
 
 class Console
 {
@@ -136,19 +66,32 @@ class Console
 		// Reads keys asynchronously
 		static void AsyncReadKey(int* output, int* condition, int valueToStop);
 
+		// Breaks read key (or getch) function
+		static void BreakReadKey();
+
 		// Hides blinking cursor in CMD
-		static void HideCursor(bool hideCursor);
+		static void HideCursor(bool hideCursor = true);
 
 		// Hides a scroll bars on down and right side in CMD
 		static void HideScrollBars(bool hideScrollBars);
 
 		// Enables ANSI escape sequences in CMD on Windows 10 1511 and higher
-		static void FixAnsiCodes();
+		static void EnableAnsiEscapeCodes();
+
+		// Enables UTF-8 I/O in CMD
+		static void EnableUTF8Output();
+		static void EnableUTF8Input();
+
+		// Enables WCHAR I/O in CMD
+		static void EnableWCHAROutput();
+		static void EnableWCHARInput();
 
 	private:
 		static HANDLE hConsole;
 		static short currentColor;
 		static short currentColorB;
+	#else
+	
 	#endif
 };
 #endif // CONSOLE_H
