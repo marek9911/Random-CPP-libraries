c4a1461531b1405bf37be611a8eecf50a20cc652
marek9911 <53997336+marek9911@users.noreply.github.com>
2023-09-28 00:24:33 +0200

    smaller update

--- RandomFunctions/RandomFunctions.cpp
+++ RandomFunctions/RandomFunctions.cpp
@@ -1,6 +1,6 @@
 #include "RandomFunctions.h"
-#include <string.h>
 #include <iostream>
+#include <Windows.h>
 
 const wchar_t* RandomFunctions::LPCSTRtoLPCWSTR(const char* str) {
 	size_t wchSize = strlen(str) * sizeof(wchar_t) + sizeof(wchar_t);
@@ -91,4 +91,128 @@ void RandomFunctions::hexDumpW(void* ptr, size_t size) {
 		std::wcout << (int)*(chptr++) << L" ";
 	}
 	std::wcout << std::dec << std::endl;
+}
+
+std::string RandomFunctions::AsciiDump(const char* bytes, size_t size) {
+	std::string ascii;
+	for (size_t i = 0; i < size; i++) {
+		if ((bytes[i] >= 32) && (bytes[i] <= 126)) {
+			ascii += bytes[i];
+		}
+		else {
+			ascii += '.';
+		}
+	}
+	return ascii;
+}
+
+std::string RandomFunctions::hexDump(const char* bytes, size_t size) {
+	#define HINIBBLE(x) (((((unsigned char)x) >> 4) | 0xF0) ^ 0xF0)
+	#define LONIBBLE(x) ((((unsigned char)x) | 0xF0) ^ 0xF0)
+	const char* arr = "0123456789ABCDEF";
+	std::string hex;
+	for (size_t i = 0; i < size; i++) {
+		hex += arr[HINIBBLE(bytes[i])];
+		hex += arr[LONIBBLE(bytes[i])];
+		hex += ' ';
+	}
+	hex.pop_back();
+	return hex;
+}
+
+int RandomFunctions::nwprintf(const char* _Format, ...) {
+	// String formatting
+	va_list args;
+	va_start(args, _Format);
+	size_t length = vsnprintf(NULL, 0, _Format, args);
+	char* buffer = (char*)malloc(length * sizeof(char) + sizeof(char));
+	if (buffer == NULL) {
+		return -1;
+	}
+	int result = vsprintf_s(buffer, length + 1, _Format, args);
+	va_end(args);
+
+	// Getting console cursor coordinates
+	HANDLE hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	COORD coord;
+	if (GetConsoleScreenBufferInfo(hConsoleOutput, &csbi)) {
+		coord = csbi.dwCursorPosition;
+	}
+	else {
+		return -1;
+	}
+
+	// Printing to the console
+	DWORD charactersWritten;
+	WriteConsoleOutputCharacterA(
+		hConsoleOutput,
+		buffer,
+		(DWORD)strlen(buffer),
+		coord,
+		&charactersWritten
+	);
+
+	// Move and calculate cursor position
+	int consoleW = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+	if ((coord.X + (SHORT)charactersWritten) >= consoleW) {
+		coord.X = ((SHORT)charactersWritten - (consoleW - coord.X));
+		coord.Y += (coord.X / consoleW) + 1;
+		coord.X %= consoleW;
+	}
+	else {
+		coord.X += (SHORT)charactersWritten;
+	}
+	SetConsoleCursorPosition(hConsoleOutput, coord);
+
+	free(buffer);
+	return charactersWritten;
+}
+int RandomFunctions::wnwprintf(const wchar_t* _Format, ...) {
+	// String formatting
+	va_list args;
+	va_start(args, _Format);
+	size_t length = vswprintf(NULL, 0, _Format, args);
+	wchar_t* buffer = (wchar_t*)malloc(length * sizeof(wchar_t) + sizeof(wchar_t));
+	if (buffer == NULL) {
+		return -1;
+	}
+	int result = vswprintf_s(buffer, length + 1, _Format, args);
+	va_end(args);
+
+	// Getting console cursor coordinates
+	HANDLE hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	COORD coord;
+	if (GetConsoleScreenBufferInfo(hConsoleOutput, &csbi)) {
+		coord = csbi.dwCursorPosition;
+	}
+	else {
+		return -1;
+	}
+
+	// Printing to the console
+	DWORD charactersWritten;
+	WriteConsoleOutputCharacterW(
+		hConsoleOutput,
+		buffer,
+		(DWORD)wcslen(buffer),
+		coord,
+		&charactersWritten
+	);
+
+	// Move and calculate cursor position
+	int consoleW = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+	if ((coord.X + (SHORT)charactersWritten) >= consoleW) {
+		coord.X = ((SHORT)charactersWritten - (consoleW - coord.X));
+		coord.Y += (coord.X / consoleW) + 1;
+		coord.X %= consoleW;
+	}
+	else {
+		coord.X += (SHORT)charactersWritten;
+	}
+	SetConsoleCursorPosition(hConsoleOutput, coord);
+
+	free(buffer);
+	return charactersWritten;
 }
--- RandomFunctions/RandomFunctions.h
+++ RandomFunctions/RandomFunctions.h
@@ -1,12 +1,21 @@
 #pragma once
+#include <string>
+#define HINIBBLE(x) (((((unsigned char)(x)) >> 4) | 0xF0) ^ 0xF0)
+#define LONIBBLE(x) ((((unsigned char)(x)) | 0xF0) ^ 0xF0)
+#define loop(start, end) for(int i = (start); i < (end); i++)
+
 class RandomFunctions
 {
 	public:
-		const wchar_t* LPCSTRtoLPCWSTR(const char* str);
-		const char* LPCWSTRtoLPCSTR(const wchar_t* wstr);
-		char BoolsToByte(bool arr[8]);
-		bool* ByteToBools(unsigned char byte);
-		void hexDumpA(void* ptr, size_t size);
-		void hexDumpW(void* ptr, size_t size);
+		static const wchar_t* LPCSTRtoLPCWSTR(const char* str);
+		static const char* LPCWSTRtoLPCSTR(const wchar_t* wstr);
+		static char BoolsToByte(bool arr[8]);
+		static bool* ByteToBools(unsigned char byte);
+		static void hexDumpA(void* ptr, size_t size);
+		static void hexDumpW(void* ptr, size_t size);
+		static std::string AsciiDump(const char* bytes, size_t size);
+		static std::string hexDump(const char* bytes, size_t size);
+		static int nwprintf(const char* _Format, ...);
+		static int wnwprintf(const wchar_t* _Format, ...);
 };
 
